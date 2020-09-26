eb7eeb38ae9277d4d630af35afc98cdabb866236
EwelinaJ10 <ewelina.jablonska93@gmail.com>
2020-09-26 12:37:24 +0200

    Add files via upload

--- scripts/getPaths.js
+++ scripts/getPaths.js
@@ -27,31 +27,44 @@ fs.readdir(directoryPath, function (err, files) {
  function getPaths(file_name){
       
     try{
-        var paths = 0
+        var n_paths = 0
         var indexes = []
+        var abs_paths = []
         fileBuffer =  fs.readFileSync(directory +  file_name, 'utf8');
         to_string = fileBuffer.toString();
         split_lines = to_string.split("\n");
         split_lines.forEach(function (line, i){
             
-            if(line.includes(" /")|| line.includes('\n' + '/' )){
-                paths ++;
-                indexes.push(i);
+            if(line.includes(" /")==true || line.includes('\n' + '/' )==true){
+                if (line.includes(" //")==false && line.includes(" /:")==false && line.includes(" /(")==false && line.includes(" /^")==false && line.includes(' /"')==false && line.includes(" / ")==false && line.includes(" /p")==false && line.includes(" />")==false && line.includes("#")==false){
+                    n_paths ++;
+                    indexes.push(i);
+                    abs_paths.push(line)
+                }
+               
+            }
+            if(line.includes("C:\\")){
+                
+                    n_paths ++;
+                    indexes.push(i);
+                    abs_paths.push(line)
+                
+               
             }
         });
-        paths_file.write((`${file_name}, ${paths} `))
-        if(indexes.length==0){
+        paths_file.write((`${file_name}, ${n_paths} `))
+        if(indexes.length==0){ 
             paths_file.write((` \n`))
         }
         else{
             paths_file.write((`, `))
         }
-        indexes.forEach(function (index, i){
-            if(i==indexes.length-1){
-                paths_file.write((`${index+1} \n`))
+        abs_paths.forEach(function (path, i){
+            if(i==abs_paths.length-1){
+                paths_file.write((`${path} \n`))
             }
             else{
-                paths_file.write((`${index+1}, `))
+                paths_file.write((`${path}, `))
             }
            
         });
